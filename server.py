ab22907daa2ac62534a89d0d631c709244d054c3
Rafael Augusto Ramos <august.rafael@gmail.com>
2026-08-19 23:18:44 -0300

    correção da posição dos elementos da tabela

--- server.py
+++ server.py
@@ -834,10 +834,9 @@ def render_planos_ideias_table():
                           <input type="checkbox" class="checkbox" />
                         </label>
                       </th>
-                      <th>Name</th>
-                      <th>Job</th>
-                      <th>Favorite Color</th>
-                      <th></th>
+                      <th>Identificação</th>
+                      <th>Tipo</th>
+                      <th>Descrição</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -850,25 +849,16 @@ def render_planos_ideias_table():
                       </th>
                       <td>
                         <div class="flex items-center gap-3">
-                          <div class="avatar">
-                            <div class="mask mask-squircle h-12 w-12">
-                              <img
-                                src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                alt="Avatar Tailwind CSS Component" />
-                            </div>
-                          </div>
                           <div>
-                            <div class="font-bold">Hart Hagerty</div>
-                            <div class="text-sm opacity-50">United States</div>
+                            <div class="font-bold">Plano de idéias</div>
+                            <div class="text-sm opacity-50 text-center">1</div>
                           </div>
                         </div>
                       </td>
                       <td>
-                        Zemlak, Daniel and Leannon
                         <br />
-                        <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                        <span class="badge badge-ghost badge-sm">Plano</span>
                       </td>
-                      <td>Purple</td>
                       <th>
                         <button class="btn btn-ghost btn-xs">details</button>
                       </th>
@@ -882,28 +872,25 @@ def render_planos_ideias_table():
                       </th>
                       <td>
                         <div class="flex items-center gap-3">
-                          <div class="avatar">
-                            <div class="mask mask-squircle h-12 w-12">
-                              <img
-                                src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-                                alt="Avatar Tailwind CSS Component" />
-                            </div>
-                          </div>
                           <div>
-                            <div class="font-bold">Brice Swyre</div>
-                            <div class="text-sm opacity-50">China</div>
+                            <div class="font-bold">OCR de palavras</div>
+                            <div class="text-sm opacity-50 text-center">2</div>
                           </div>
                         </div>
                       </td>
-                      <td>
-                        Carroll Group
                         <br />
-                        <span class="badge badge-ghost badge-sm">Tax Accountant</span>
+                      <td>  
+                        <span class="badge badge-ghost badge-sm">Plano</span>
                       </td>
-                      <td>Red</td>
-                      <th>
+                      </td>     
+                        <span class="" style="display: ruby-text;">
+                          Criar um aplicativo que faça a leitura de palavras em imagens, 
+                          coloque bordas em volta das palavras reconhecidas e as torne 
+                          selecionaveis
+                        </span>
                         <button class="btn btn-ghost btn-xs">details</button>
-                      </th>
+                      
+                      </td>
                     </tr>
                     <!-- row 3 -->
                     <tr>
@@ -914,57 +901,16 @@ def render_planos_ideias_table():
                       </th>
                       <td>
                         <div class="flex items-center gap-3">
-                          <div class="avatar">
-                            <div class="mask mask-squircle h-12 w-12">
-                              <img
-                                src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-                                alt="Avatar Tailwind CSS Component" />
-                            </div>
-                          </div>
                           <div>
-                            <div class="font-bold">Marjy Ferencz</div>
-                            <div class="text-sm opacity-50">Russia</div>
+                            <div class="font-bold">Novo Plano</div>
+                            <div class="text-sm opacity-50 text-center">3</div>
                           </div>
                         </div>
                       </td>
                       <td>
-                        Rowe-Schoen
                         <br />
-                        <span class="badge badge-ghost badge-sm">Office Assistant I</span>
+                        <span class="badge badge-ghost badge-sm">Plano</span>
                       </td>
-                      <td>Crimson</td>
-                      <th>
-                        <button class="btn btn-ghost btn-xs">details</button>
-                      </th>
-                    </tr>
-                    <!-- row 4 -->
-                    <tr>
-                      <th>
-                        <label>
-                          <input type="checkbox" class="checkbox" />
-                        </label>
-                      </th>
-                      <td>
-                        <div class="flex items-center gap-3">
-                          <div class="avatar">
-                            <div class="mask mask-squircle h-12 w-12">
-                              <img
-                                src="https://img.daisyui.com/images/profile/demo/5@94.webp"
-                                alt="Avatar Tailwind CSS Component" />
-                            </div>
-                          </div>
-                          <div>
-                            <div class="font-bold">Yancy Tear</div>
-                            <div class="text-sm opacity-50">Brazil</div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        Wyman-Ledner
-                        <br />
-                        <span class="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-                      </td>
-                      <td>Indigo</td>
                       <th>
                         <button class="btn btn-ghost btn-xs">details</button>
                       </th>
@@ -974,9 +920,9 @@ def render_planos_ideias_table():
                   <tfoot>
                     <tr>
                       <th></th>
-                      <th>Name</th>
-                      <th>Job</th>
-                      <th>Favorite Color</th>
+                      <th>Identificação</th>
+                      <th>Tipo</th>
+                      <th>Descrição</th>
                       <th></th>
                     </tr>
                   </tfoot>
